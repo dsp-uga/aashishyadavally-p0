1f6d9dc9d5d7550b08acefd3288d4b10773bfbb3
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-01-22 12:30:52 -0500

    added function which returns computed idf for all words in word dictionary

--- src/sp4_functions.py
+++ src/sp4_functions.py
@@ -14,3 +14,20 @@ def get_word_dictionary(wordcountlist):
         word_dictionary.extend(wordcountlist[i])
     return word_dictionary
 
+
+def get_idf(wordcountlist):
+    """
+    Computes Inverse Document Frequency (IDF) from the second parameter in the
+    tuple associated with a particular key, both of which are returned by
+    using the combineByKey() method on the RDD
+
+    :param wordcountlist:
+    :return idf:
+    """
+    sc1 = SparkContext.getOrCreate()
+    data = sc1.parallelize(get_word_dictionary(wordcountlist)) \
+        .combineByKey(lambda value: (value, 1),
+                      lambda x, value: (x[0] + value, x[1] + 1),
+                      lambda x, y: (x[0] + y[0], x[1] + y[1]))
+    idf = data.map(lambda x: (x[0], math.log(8 / x[1][1]))).collectAsMap()
+    return idf
